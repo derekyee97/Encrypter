f348014fd8da1afc47a62c69b05acecdba42721a
Derek Yee <dereky007@gmail.com>
2020-02-08 11:23:24 -0800

    added caesar cipher encryption

--- Encrypter/main.cpp
+++ Encrypter/main.cpp
@@ -1,5 +1,8 @@
 #include<string>
 #include<iostream>
+#include<windows.h>
+#include <iostream>
+#include<fstream>
 using namespace std;
 
 int main()
@@ -24,6 +27,43 @@ int main()
 			cout << "Quitting...";
 			exit(0);
 		}
+		else if (choice == 1)
+		{
+			cout << "Please type in the file path of the file you would like to encrypt.\n"; 
+			cout << "Example: ' C:\\Users\\Documents\\example.txt \n"; //have to use double backslash to indicate path for c++ 
+			string fileName;
+			cin.ignore();
+			getline(cin, fileName);
+			fstream input;
+			ofstream output;
+			input.open(fileName); 
+			if (!input)  //if error in finding file 
+			{
+				cout << "File not found...\n";
+				continue; 
+			}
+			output.open("output.txt"); 
+			if (!output)
+			{
+				cout << "Error in creating output file...\n"; 
+				continue;
+			}
+			char curr;
+			while (input.eof() == 0)
+			{
+				input >> curr; 
+				curr = curr + 100; //caesar cipher 
+				output << curr; 
+			}
+			input.close();
+			output.close();
+			cout << "Encrypted using caesar cipher in output file labeled output.txt\n";
+		}
+		else if (choice == 2)
+		{
+
+		}
 		
 	}
+	return 0;
 }
